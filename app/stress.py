35eb2ed47203c88db63cf6eebae1400554e0aa6d
alia4lisa <alisaalia74@gmail.com>
2025-01-11 15:11:33 +0800

    stress commit

--- app/stress.py
+++ app/stress.py
@@ -9,6 +9,13 @@ def calculate_stress_score(answers):
        (slot overwhelmed)
        (slot irritability)
        (slot tension)
+       (slot lonely)
+       (slot forgetful)
+       (slot unhealthy)
+       (slot uncontrolled)
+       (slot stressful)
+       (slot lost_interest)
+       (slot sensitive)
     )
     """)
 
@@ -24,6 +31,13 @@ def calculate_stress_score(answers):
        (slot cf1 (default 0))
        (slot cf2 (default 0))
        (slot cf3 (default 0))
+       (slot cf4 (default 0))
+       (slot cf5 (default 0))
+       (slot cf6 (default 0))
+       (slot cf7 (default 0))
+       (slot cf8 (default 0))
+       (slot cf9 (default 0))
+       (slot cf10 (default 0))
     )
     """)
 
@@ -69,10 +83,101 @@ def calculate_stress_score(answers):
     """)
 
     env.build("""
-    (defrule final-check
-       (cf-values (cf1 ?cf1) (cf2 ?cf2) (cf3 ?cf3))
+    (defrule check-lonely
+       (questionnaire (lonely ?answer))
+       ?cf <- (cf-values (cf4 ?cf4))
        =>
-       (bind ?total (+ ?cf1 ?cf2 ?cf3))
+       (if (eq ?answer "yes") then
+          (modify ?cf (cf4 0.95))
+       else
+          (modify ?cf (cf4 0.05))
+       )
+    )
+    """)
+
+    env.build("""
+    (defrule check-forgetful
+       (questionnaire (forgetful ?answer))
+       ?cf <- (cf-values (cf5 ?cf5))
+       =>
+       (if (eq ?answer "yes") then
+          (modify ?cf (cf5 0.95))
+       else
+          (modify ?cf (cf5 0.05))
+       )
+    )
+    """)
+
+    env.build("""
+    (defrule check-unhealthy
+       (questionnaire (unhealthy ?answer))
+       ?cf <- (cf-values (cf6 ?cf6))
+       =>
+       (if (eq ?answer "yes") then
+          (modify ?cf (cf6 0.95))
+       else
+          (modify ?cf (cf6 0.05))
+       )
+    )
+    """)
+
+    env.build("""
+    (defrule check-uncontrolled
+       (questionnaire (uncontrolled ?answer))
+       ?cf <- (cf-values (cf7 ?cf7))
+       =>
+       (if (eq ?answer "yes") then
+          (modify ?cf (cf7 0.95))
+       else
+          (modify ?cf (cf7 0.05))
+       )
+    )
+    """)
+
+    env.build("""
+    (defrule check-stressful
+       (questionnaire (stressful ?answer))
+       ?cf <- (cf-values (cf8 ?cf8))
+       =>
+       (if (eq ?answer "yes") then
+          (modify ?cf (cf8 0.95))
+       else
+          (modify ?cf (cf8 0.05))
+       )
+    )
+    """)
+
+    env.build("""
+    (defrule check-lost_interest
+       (questionnaire (lost_interest ?answer))
+       ?cf <- (cf-values (cf9 ?cf9))
+       =>
+       (if (eq ?answer "yes") then
+          (modify ?cf (cf9 0.95))
+       else
+          (modify ?cf (cf9 0.05))
+       )
+    )
+    """)
+
+    env.build("""
+    (defrule check-sensitive
+       (questionnaire (sensitive ?answer))
+       ?cf <- (cf-values (cf10 ?cf10))
+       =>
+       (if (eq ?answer "yes") then
+          (modify ?cf (cf10 0.95))
+       else
+          (modify ?cf (cf10 0.05))
+       )
+    )
+    """)
+
+    env.build("""
+    (defrule final-check
+       (cf-values (cf1 ?cf1) (cf2 ?cf2) (cf3 ?cf3) (cf4 ?cf4) (cf5 ?cf5) (cf6 ?cf6) (cf7 ?cf7) (cf8 ?cf8) (cf9 ?cf9) (cf10 ?cf10))
+       =>
+       (bind ?total (+ ?cf1 ?cf2 ?cf3 ?cf4 ?cf5 ?cf6 ?cf7 ?cf8 ?cf9 ?cf10))
        (bind ?average (/ ?total 3))
        (if (<= ?average 0.25) then
           (assert (stress-value (cf ?average) (value "No Stress")))
@@ -99,7 +204,7 @@ def calculate_stress_score(answers):
     )
     """)
 
-    env.assert_string(f'(questionnaire (overwhelmed "{answers.get("overwhelmed")}") (irritability "{answers.get("irritability")}") (tension "{answers.get("tension")}") )')
+    env.assert_string(f'(questionnaire (overwhelmed "{answers.get("overwhelmed")}") (irritability "{answers.get("irritability")}") (tension "{answers.get("tension")}") (lonely "{answers.get("lonely")}") (forgetful "{answers.get("forgetful")}") (unhealthy "{answers.get("unhealthy")}") (uncontrolled "{answers.get("uncontrolled")}") (stressful "{answers.get("stressful")}") (lost_interest "{answers.get("lost_interest")}") (sensitive "{answers.get("sensitive")}"))')
 
     env.run()
 
--- templates/stress.php
+++ templates/stress.php
@@ -22,6 +22,34 @@
         <label>Do you experience headaches or physical tension?</label><br>
         <input type="radio" name="tension" value="yes" required> Yes<br>
         <input type="radio" name="tension" value="no" required> No<br><br>
+
+        <label>Do you feel lonely or isolated?</label><br>
+        <input type="radio" name="lonely" value="yes" required> Yes<br>
+        <input type="radio" name="lonely" value="no" required> No<br><br>
+
+        <label>Do you experience memory problems or difficulty making decisions?</label><br>
+        <input type="radio" name="forgetful" value="yes" required> Yes<br>
+        <input type="radio" name="forgetful" value="no" required> No<br><br>
+
+        <label>Do you engage in unhealthy coping mechanisms (e.g., excessive alcohol or drug use)?</label><br>
+        <input type="radio" name="unhealthy" value="yes" required> Yes<br>
+        <input type="radio" name="unhealthy" value="no" required> No<br><br>
+
+        <label>Felt that you were unable to control important things in your life?</label><br>
+        <input type="radio" name="uncontrolled" value="yes" required> Yes<br>
+        <input type="radio" name="uncontrolled" value="no" required> No<br><br>
+
+        <label>On a scale of 1 to 10, how would you rate your work stress level?</label><br>
+        <input type="radio" name="stressful" value="yes" required> Yes<br>
+        <input type="radio" name="stressful" value="no" required> No<br><br>
+
+        <label>How much have you noticed a decrease in your personal productivity?</label><br>
+        <input type="radio" name="lost_interest" value="yes" required> Yes<br>
+        <input type="radio" name="lost_interest" value="no" required> No<br><br>
+
+        <label>how sensitive are you to noise or distractions?</label><br>
+        <input type="radio" name="sensitive" value="yes" required> Yes<br>
+        <input type="radio" name="sensitive" value="no" required> No<br><br>
         
         <button type="submit">See Results</button>
     </form>
