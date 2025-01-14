f2d624881cfb387f75a45be3c7cdedb028e6ff13
ayesharizani <1211103435@student.mmu.edu.my>
2025-01-15 03:49:57 +0800

    Update depression.php

--- templates/depression.php
+++ templates/depression.php
@@ -11,7 +11,7 @@
     <h1>Step 1: Depression</h1>
 
     <form action="/depression" method="post">
-        <label>Do you have sleep issues?</label><br>
+        <label>How often have you had trouble sleeping over the last two weeks?</label><br>
         <input type="radio" name="sleep_issues" value="0.0" required> Never<br>
         <input type="radio" name="sleep_issues" value="0.2" required> Rarely<br>
         <input type="radio" name="sleep_issues" value="0.4" required> Sometimes<br>
@@ -19,21 +19,45 @@
         <input type="radio" name="sleep_issues" value="0.8" required> Very often<br>
         <input type="radio" name="sleep_issues" value="1.0" required> Always<br><br>
 
-        <label>Have you experienced appetite changes?</label><br>
-        <input type="radio" name="appetite_changes" value="0.0" required> Never<br>
-        <input type="radio" name="appetite_changes" value="0.2" required> Rarely<br>
-        <input type="radio" name="appetite_changes" value="0.4" required> Sometimes<br>
-        <input type="radio" name="appetite_changes" value="0.6" required> Often<br>
-        <input type="radio" name="appetite_changes" value="0.8" required> Very often<br>
-        <input type="radio" name="appetite_changes" value="1.0" required> Always<br><br>
+        <label>How much have you felt a lack of energy over the last two weeks?</label><br>
+        <input type="radio" name="energy" value="0.0" required> Never<br>
+        <input type="radio" name="energy" value="0.2" required> Rarely<br>
+        <input type="radio" name="energy" value="0.4" required> Sometimes<br>
+        <input type="radio" name="energy" value="0.6" required> Often<br>
+        <input type="radio" name="energy" value="0.8" required> Very often<br>
+        <input type="radio" name="energy" value="1.0" required> Always<br><br>
 
-        <label>Are you having trouble concentrating?</label><br>
+        <label>How much have you noticed changes in your appetite over the last two weeks?</label><br>
+        <input type="radio" name="appetite" value="0.0" required> Never<br>
+        <input type="radio" name="appetite" value="0.2" required> Rarely<br>
+        <input type="radio" name="appetite" value="0.4" required> Sometimes<br>
+        <input type="radio" name="appetite" value="0.6" required> Often<br>
+        <input type="radio" name="appetite" value="0.8" required> Very often<br>
+        <input type="radio" name="appetite" value="1.0" required> Always<br><br>
+
+        <label>To what extent have you had difficulty concentrating over the last two weeks?</label><br>
         <input type="radio" name="concentration" value="0.0" required> Never<br>
         <input type="radio" name="concentration" value="0.2" required> Rarely<br>
         <input type="radio" name="concentration" value="0.4" required> Sometimes<br>
         <input type="radio" name="concentration" value="0.6" required> Often<br>
         <input type="radio" name="concentration" value="0.8" required> Very often<br>
         <input type="radio" name="concentration" value="1.0" required> Always<br><br>
+
+        <label>How much have you felt passive or lacked motivation to engage in activities over the last two weeks?</label><br>
+        <input type="radio" name="behaviour" value="0.0" required> Never<br>
+        <input type="radio" name="behaviour" value="0.2" required> Rarely<br>
+        <input type="radio" name="behaviour" value="0.4" required> Sometimes<br>
+        <input type="radio" name="behaviour" value="0.6" required> Often<br>
+        <input type="radio" name="behaviour" value="0.8" required> Very often<br>
+        <input type="radio" name="behaviour" value="1.0" required> Always<br><br>
+
+        <label>Can you rate how much you wanted to be alone in past two weeks?</label><br>
+        <input type="radio" name="social_isolation" value="0.0" required> Never<br>
+        <input type="radio" name="social_isolation" value="0.2" required> Rarely<br>
+        <input type="radio" name="social_isolation" value="0.4" required> Sometimes<br>
+        <input type="radio" name="social_isolation" value="0.6" required> Often<br>
+        <input type="radio" name="social_isolation" value="0.8" required> Very often<br>
+        <input type="radio" name="social_isolation" value="1.0" required> Always<br><br>
 
         <button type="submit">Next</button>
     </form>
